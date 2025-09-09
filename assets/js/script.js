223a7e6af4ec89a8359f9b77bf64aad9f6795f63
adrienb39 <adrienb39@yahoo.com>
2025-09-09 11:18:25 +0200

    modification du nouveau portfolio

--- assets/js/script.js
+++
@@ -1,6 +0,0 @@
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-if (prefersDarkScheme.matches) {
-    // le mode sombre est activé
-} else {
-    // le mode sombre n'est pas activé
-}
